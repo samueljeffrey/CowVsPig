0c063264bb6ff7c4f69cfb232ca8980ff5e6efad
samueljeffrey <77203993+samueljeffrey@users.noreply.github.com>
2021-02-14 14:52:40 +0000

    Reversing unsuccessful start button fix

--- script.js
+++ script.js
@@ -51,7 +51,6 @@ const original = function() {
   body.style.background = "linear-gradient(to right, #9003fc, #fc0335)";
   resetButton.classList.add("button-disabled");
   startButton.classList.remove("button-disabled");
-  startButton.disabled = false;
   turnSign.classList.add("hidden");
   instructions.classList.remove("hidden");
   for (let i = 0; i < allImages.length; i++) {
@@ -122,7 +121,6 @@ original();
 
 startButton.addEventListener("click", function() {
   startButton.classList.add("button-disabled");
-  startButton.disabled;
   resetButton.classList.remove("button-disabled");
   currentPlayer = players[(Math.floor(Math.random()*2))];
   turnSign.textContent = `${currentPlayer}'s turn`;
